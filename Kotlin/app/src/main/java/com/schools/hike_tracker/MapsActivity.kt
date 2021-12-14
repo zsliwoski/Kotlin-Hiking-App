e5be0d82553373a14cc0873f34631bf9810e3cd5
zarek <zsliwoski@gmail.com>
2021-12-13 19:07:19 -0700

    added layout and gps

--- Kotlin/app/src/main/java/com/schools/hike_tracker/MapsActivity.kt
+++ Kotlin/app/src/main/java/com/schools/hike_tracker/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.schools.hike_tracker
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,14 +16,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.schools.hike_tracker.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var cPoints : List<LatLng>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
@@ -27,6 +40,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        //tvGpsLocation = findViewById(R.id.textView)
+        //tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+    }
+    /*override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }*/
 
     /**
      * Manipulates the map once available.
@@ -44,5 +79,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+    fun updateMapPosition(pos : LatLng){
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+    }
+    fun clearMapMarkers(){
+        mMap.clear()
+    }
+
+    fun addMapMarkers(points : List<LatLng>){
+        clearMapMarkers();
+
+        val line = PolylineOptions().clickable(true)
+
+        //add points to our line
+        for (p in points) {
+            line.add(p)
+        }
+        mMap.addPolyline(line)
+
+        //map our start
+        mMap.addMarker(MarkerOptions().position(points.first()).title("Start"));
+        //map our end
+        mMap.addMarker(MarkerOptions().position(points.last()).title("End"));
+        //move to our first point
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(points[0]));
+
     }
 }
